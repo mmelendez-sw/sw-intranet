bbe4383d00668837c62fff85757f5428cbf96ed7
Matt Melendez <mmelendez@symphonywireless.com>
2024-11-06 16:20:06 -0500

    add favicons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,12 +7,12 @@ const Header: React.FC = () => {
     <header className="header">
       <img src={logo} alt={'Logo'} className="logo-image" />
       <nav className="nav-bar">
-        <a href="/">Home</a>
-        <a href="/acquisitions">Acquisitions</a>
-        <a href="/Pricing">Pricing</a>
-        <a href="/legal">Legal</a>
-        <a href="/hr">HR</a>
-        <a href="/it">IT</a>
+        <i className="fa-solid fa-house"></i> <a href="/"> Home</a>
+        <i className="fa-solid fa-wifi"></i><a href="/acquisitions"> Acquisitions</a>
+        <i className="fa-solid fa-wallet"></i><a href="/pricing"> Pricing</a>
+        <i className="fa-brands fa-readme"></i><a href="/legal"> Legal</a>
+        <i className="fa-solid fa-user"></i><a href="/hr"> HR</a>
+        <i className="fa-solid fa-laptop"></i><a href="/it"> IT</a>
       </nav>
       <div className="user">Welcome, Matt!</div>
     </header>
