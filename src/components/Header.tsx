7d064ad6c7486bf4e991cd20fda080dd586f9ec7
Matt Melendez <mmelendez@symphonywireless.com>
2024-11-06 11:09:32 -0500

    update header links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,11 @@ const Header: React.FC = () => {
       <div className="logo">COMPANY LOGO</div>
       <nav className="nav-bar">
         <a href="/">Home</a>
-        <a href="/news">News</a>
-        <a href="/resources">Resources</a>
-        <a href="/contact">Contact</a>
+        <a href="/acquisitions">Acquisitions</a>
+        <a href="/Pricing">Pricing</a>
+        <a href="/legal">Legal</a>
+        <a href="/hr">HR</a>
+        <a href="/it">IT</a>
       </nav>
       <div className="user">Welcome, Matt!</div>
     </header>
