610187d674c056d2e0f86da5a59087101e14a653
Matt Melendez <mmelendez@symphonywireless.com>
2024-11-11 10:51:33 -0500

    update msal config to utilize client id

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,6 +1,6 @@
 export const msalConfig = {
     auth: {
-      clientId: "<Your Client ID>",
+      clientId: "543ae09d-95e7-47bb-b679-e4428c20918e",
       authority: "https://login.microsoftonline.com/63fbe43e-8963-4cb6-8f87-2ecc3cd029b4",
       redirectUri: "http://localhost:3000",
     },
