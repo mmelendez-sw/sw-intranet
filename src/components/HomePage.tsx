bbd69c087ecc2ec05997a3405275d9d452032303
Matt Melendez <mmelendez@symphonywireless.com>
2024-11-14 12:14:39 -0500

    update position

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -12,10 +12,10 @@ const HomePage: React.FC = () => {
       <PhotoStream /> 
       <div className="content-container">
         <div className="left-section">
-          <div className="report-section">
+          {/* <div className="report-section">
             <h2>Current Analytics</h2>
             <Carousel />
-          </div>
+          </div> */}
           <div className="doubles-section">
             <div className="column-container">
               <div className="press-section">
@@ -31,10 +31,19 @@ const HomePage: React.FC = () => {
               </div>
             </div>
           </div>
+          <div className="report-section">
+            <h2>Current Analytics</h2>
+            <Carousel />
+          </div>
         </div>
 
 
-        <div className="right-section">    
+        <div className="right-section">  
+          <div className="it-section">
+            <h2>IT Updates</h2>
+            <p>New Laptops will be distibuted in batches</p>
+            <p>Do not fall bait to any phishing links!</p>
+          </div>  
           <div className="announcement-section">
             <h2>Important Dates/Holidays</h2>
             <p><span className="date">11/28:</span>  Thanksgiving Day</p>
@@ -42,11 +51,6 @@ const HomePage: React.FC = () => {
             <p><span className="date">12/5:</span>  Symphony Wireless Holiday Party</p>
             <p><span className="date">12/25:</span>  Christmas Day</p>
             <p><span className="date">1/1:</span>  New Years Day</p>
-          </div>
-          <div className="it-section">
-            <h2>IT Updates</h2>
-            <p>New Laptops will be distibuted in batches</p>
-            <p>Do not fall bait to any phishing links!</p>
           </div>
           <div className="calendar-section">
             <h2>Calendar</h2>
