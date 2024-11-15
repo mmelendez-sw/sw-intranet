3d8b1ed3fd0aee1628e582a1cd826ca3e596017c
Matt Melendez <mmelendez@symphonywireless.com>
2024-11-15 16:59:34 -0500

    update photo stream css

--- src/components/PhotoStream.tsx
+++ src/components/PhotoStream.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../../styles/photo-stream.css';
+import '../../styles/photo-stream.css'; // Adjust path as needed
 
 import image1 from '../../images/site_1.jpg';
 import image2 from '../../images/site_2.jpg';
@@ -9,23 +9,23 @@ import image5 from '../../images/site_5.jpg';
 const images = [image1, image2, image5, image4];
 
 const PhotoStream: React.FC = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
+useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, 10000); // Change every 10 seconds
 
     return () => clearInterval(interval); // Cleanup the interval on component unmount
-  }, []);
+}, []);
 
-  return (
+return (
     <div className="photo-stream">
-      <img
-        src={images[currentIndex]}
-        alt={`Slide ${currentIndex}`}
-        className="photo-stream-image"
-      />
+        <img
+            src={images[currentIndex]}
+            alt={`Slide ${currentIndex}`}
+            className="photo-stream-image"
+        />
     </div>
   );
 };
