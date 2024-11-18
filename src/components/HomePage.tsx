17f976785d1efbe2384dbb4da5d9ec4213b29b39
Matt Melendez <mmelendez@symphonywireless.com>
2024-11-18 09:52:02 -0500

    input dates on homepage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -18,11 +18,17 @@ const HomePage: React.FC = () => {
           <section className="events">
             <div className="event">
               <h2>Company Events</h2>
-              <p>Details on upcoming company events</p>
+              <p>11/21: November Bagel Breakfast</p>
+              <p>12/12: Ugly Sweater Day</p>
+              <p>12/5: Symphony Wireless Holiday Party</p>
+              <p>12/21: December Bagel Breakfast</p>
             </div>
             <div className="event">
               <h2>Conferences & Events</h2>
-              <p>Industry events and conference dates</p>
+              <p>11/20: New Jersey Wireless Association Holiday Social</p>
+              <p>11/4: California Wireless Association SoCal Holiday Party</p>
+              <p>12/11-12/12: ICSC New York</p>
+              <p>12/13: Florida Wireless Association Charity Social and Golf Tournament</p>
             </div>
             <div className="event">
               <h2>Conferences & Events</h2>
@@ -42,7 +48,8 @@ const HomePage: React.FC = () => {
           </section>
           <section className="updates">
             <h2>IT Updates</h2>
-            <p>Important announcements from IT</p>
+            <p>Make Align appointment if you have a new laptop</p>
+            <p>Do not click any phishing links</p>
           </section>
           <section className="important-dates">
             <h2>Important Dates</h2>
