d5bcd86be40c3b09ed332968e832019dbc8bf390
Matt Melendez <mmelendez@symphonywireless.com>
2024-11-19 09:45:14 -0500

    fix nesting in authConfig

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,15 +1,15 @@
 export const msalConfig = {
-    auth: {
-      clientId: "543ae09d-95e7-47bb-b679-e4428c20918e",
-      authority: "https://login.microsoftonline.com/63fbe43e-8963-4cb6-8f87-2ecc3cd029b4",
-      redirectUri: "https://intranet.symphonywireless.com",
-    },
-    cache: {
-      cacheLocation: "localStorage", // Use localStorage for persistence
-      storeAuthStateInCookie: true, // If you want cookies, set to true
-    },
-  };
-  
-  export const loginRequest = {
-    scopes: ["User.Read"], // Adjust the scopes based on what you need
-  };
+  auth: {
+    clientId: "543ae09d-95e7-47bb-b679-e4428c20918e",
+    authority: "https://login.microsoftonline.com/63fbe43e-8963-4cb6-8f87-2ecc3cd029b4",
+    redirectUri: "https://intranet.symphonywireless.com",
+  },
+  cache: {
+    cacheLocation: "localStorage", // Use localStorage for persistence
+    storeAuthStateInCookie: true, // If you want cookies, set to true
+  },
+};
+
+export const loginRequest = {
+  scopes: ["User.Read"], // Adjust the scopes based on what you need
+};
