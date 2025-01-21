5008fad7ce28edad85e2264baa82696c76d45f7b
Matt Melendez <mmelendez@symphonywireless.com>
2025-01-21 09:31:54 -0500

    remove dates

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -122,8 +122,6 @@ const HomePage: React.FC<HomePageProps> = ({ isAuthenticated }) => {
               <section className="important-dates">
                 <h2>Important Dates</h2>
                 <ul>
-                  <li>1/16 - Bagel Breakfast</li>
-                  <li>1/20 - Martin Luther King Jr. Day</li>
                   <li>1/23 - 2025 Kick Off</li>
                   <li>2/17 - President's Day</li>
                 </ul>
