973290e024651d01fa7e6dbe1b0093c5ee0729f5
Matt Melendez <Mmelendez@symphonywireless.com>
2025-04-03 12:53:58 -0400

    add two links to reports menu

--- src/components/Reports.tsx
+++ src/components/Reports.tsx
@@ -13,12 +13,20 @@ const reports = [
     description: 'A comprehensive look at All Symphony Towers Infrastructure Acquisitions broken down by month, quarter, and year.',
     link: 'https://app.powerbi.com/links/PDJWKnYPlL?ctid=63fbe43e-8963-4cb6-8f87-2ecc3cd029b4&pbi_source=linkShare&bookmarkGuid=7983c722-1351-4091-a28b-023c2d74063b',
   },
+  {title: 'Asset Management TK Sites',
+    description: 'A comprehensive look at TK High Rent Relocation Sites and their status.',
+    link: 'https://app.powerbi.com/links/ArNJaolb9U?ctid=63fbe43e-8963-4cb6-8f87-2ecc3cd029b4&pbi_source=linkShare',
+  },  
+  {title: 'Easement and Tower Combined Report',
+      description: 'A comprehensive look at Easements and Towers combined.',
+      link: 'https://app.powerbi.com/links/EcIcSqZiXq?ctid=63fbe43e-8963-4cb6-8f87-2ecc3cd029b4&pbi_source=linkShare',
+  },
   {
     // title: 'Daily Acquisitions Summary',
     // description: 'A comprehensive look at the Symphony Towers Infrastructure Daily Acquisitions.',
     // link: 'https://app.powerbi.com/links/5E9lu5wCmG?ctid=63fbe43e-8963-4cb6-8f87-2ecc3cd029b4&pbi_source=linkShare&bookmarkGuid=d608bae3-6929-4c8f-995b-bb0c0b7cf15a',
   },
-  {},  {},  {},  {},  {},  {},  {},  {},  {},  {},  {}
+    {},  {},  {},  {},  {},  {},  {},  {},  {}
 ];
 
 const TechnologyReports: React.FC = () => {
