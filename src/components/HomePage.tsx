de7cc9abd3e6ff5f89a240100827879216174b1e
Matt Melendez <Mmelendez@symphonywireless.com>
2026-02-03 09:14:55 -0500

    update finance seats

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,7 +4,7 @@ import '../../styles/home-page.css';
 
 import img3 from '../../images/site_3.jpg';
 import img4 from '../../images/coat.jpg';
-import img7 from '../../images/quiz.jpg'
+import img7 from '../../images/all-hands.jpg'
 import img9 from '../../images/site_2.jpg'
 import img10 from '../../images/alida.jpg'
 import img11 from '../../images/santa.jpg'
@@ -55,9 +55,9 @@ const HomePage: React.FC<HomePageProps> = ({ isAuthenticated }) => {
               <div className="card odd-card">
                 <img src={img7} alt="City Harvest Volunteer Event" className="card-image" />
                 <div className="card-text">
-                  <h2>Halloween Team Building</h2>
+                  <h2>Finance Team Seats Secured</h2>
                   <ul>
-                      <li>Spooktacular times at our Halloween celebration! Thanks to everyone who joined in the fun and brought their creativity to life.</li>
+                      <li>The Finance team’s seats are secured and are numbers ready for the All Hands Meeting.</li>
                     </ul>
                 </div>
               </div>
