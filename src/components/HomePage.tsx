1f4b694858930cc0b5fd43e486c88b2da113fe47
Matt Melendez <Mmelendez@symphonywireless.com>
2026-03-09 14:43:22 -0400

    move emp app photo lower

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -102,7 +102,7 @@ const HomePage: React.FC<HomePageProps> = ({ isAuthenticated }) => {
                   srcSet={`${img10Sm} 480w, ${img10Md} 900w, ${img10} 1200w`}
                   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                   alt="Employee Appreciation Day Celebration"
-                  className="card-image"
+                  className="card-image employee-appreciation-image"
                 />
                 <div className="card-text">
                   <h2>Employee Appreciation Day Celebration</h2>
