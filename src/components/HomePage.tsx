966b6e961b7230159a7b2c208374312784646856
Serena <shuang@symphonyinfra.com>
2026-07-27 11:42:23 -0400

    new company progress

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,7 +4,7 @@ import '../../styles/home-page.css';
 import { UserInfo } from '../types/user';
 // import { PowerbiService, PowerbiEmbedToken } from '../services/powerbiService';
 import howBanner from '../../images/H.O.W.-banner.png';
-import companyProgressImage from '../../images/companyprog.png';
+import companyProgressImage from '../../images/companyprog7/27.png';
 
 // const powerbiEmbedService = new service.Service(
 //   factories.hpmFactory,
